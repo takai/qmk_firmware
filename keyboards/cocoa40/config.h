d8d869f082d5b86dcc4edf71dfb9b35d3aaba49c
Naoto Takai <takai@recompile.net>
2019-03-29 06:40:30 +0900

    Update keymaps

--- keyboards/cocoa40/config.h
+++ keyboards/cocoa40/config.h
@@ -42,9 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_ROW_PINS_RIGHT { F4, F5, F7, F6 }
 #define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, D0, D1 }
-#define MATRIX_COL_PINS_RIGHT { B5, B4, E6, D7, C6, D4, D0, D1 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
